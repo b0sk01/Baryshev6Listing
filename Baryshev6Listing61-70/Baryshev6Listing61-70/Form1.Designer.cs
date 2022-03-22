4fc5fe6400f29c192c7cd62c05ad0f9c808db662
b0sk01 <82368596+b0sk01@users.noreply.github.com>
2022-03-22 16:37:21 +0300

    Add files via upload

---
+++ Baryshev6Listing61-70/Baryshev6Listing61-70/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Baryshev6Listing61_70
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(538, 35);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(200, 100);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "6.61. Добавление новой вкладки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "6.62. Удаление вкладки ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 67);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "6.63. Вставка вкладки в определенную позицию ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(538, 141);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(200, 40);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(539, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // performanceCounter1
+            // 
+            this.performanceCounter1.CategoryName = "Processor";
+            this.performanceCounter1.CounterName = "% Processor Time";
+            this.performanceCounter1.InstanceName = "_Total";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 231);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 67);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "6.64. Проверка загрузки процессора";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 304);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 67);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "6.65. Скрытие свойства или метода от IntelliSense ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 377);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(144, 67);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "6.66. Скрытие свойств и событий из редактора свойств";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(162, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(144, 67);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "6.67. Скрытие некоторых свойств и событий элемента управления ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Baryshev6Listing61-70/Baryshev6Listing61-70/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace Baryshev6Listing61_70
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Добавляем новую вкладку
+            tabControl1.Controls.Add(new TabPage("Новая вкладка"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Удалить выбранную вкладку
+            tabControl1.Controls.Remove(tabControl1.SelectedTab);
+        }
+
+        /// <summary>
+        /// Вставляет вкладку в заданную позицию
+        /// </summary>
+        /// <param name="tabNumber">Номер вкладки</param>
+        /// <param name="tabControl">Элемент TabControl</param>
+        private void InsertTab(int tabNumber, ref TabControl tabControl)
+        {
+            int counter = tabControl.Controls.Count;
+            if (tabNumber < 0 || tabNumber > counter)
+            {
+                tabControl.Controls.Add(new TabPage("Вкладка"));
+                return;
+            }
+            int target = tabControl.SelectedIndex;
+            // сохраняем существующие ярлычки и очищаем элементы
+            Control[] c = new Control[counter];
+            tabControl.Controls.CopyTo(c, 0);
+            tabControl.Controls.Clear();
+            // Добавляем ярлычки до вставляемого элемента
+            for (int i = 0; i < target; ++i)
+                tabControl.Controls.Add(c[i]);
+            // Вставляем свой ярлычок
+            tabControl.Controls.Add(new TabPage("Вставленная вкладка"));
+            // Добавляем ярлычки после вставляемого элемента
+            for (int i = target; i < counter; ++i)
+                tabControl.Controls.Add(c[i]);
+            // Выбираем вставленную вкладку
+            tabControl.SelectedIndex = target;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Добавляем ярлычок во вторую позицию
+            InsertTab(2, ref tabControl1);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = (int)(performanceCounter1.NextValue());
+            label1.Text = "Загрузка процессора: " +
+            progressBar1.Value.ToString() + "%";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //...
+        string firstNameValue;
+        [EditorBrowsableAttribute(EditorBrowsableState.Never)]
+        public string FirstName
+        {
+            get
+            {
+                return firstNameValue;
+            }
+            set
+            {
+                firstNameValue = value;
+            }
+        }
+        //...
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
